7c14e8540fe17d309b2e047bcac32b991c64e2fa
jeff-PC <jefferyfrederic@gmail.com>
2023-04-27 00:33:35 -0700

    greyscale added

--- javascript/analyzer.js
+++ javascript/analyzer.js
@@ -20,3 +20,26 @@ export function getAverageRGB(imageDataBlock) {
 }
 
 
+/**
+ * Transforms all image data into black and 
+ * white using the LUMINOSITY method. This
+ * is the best method to turn an image in
+ * a greyscale. 
+ * https://www.baeldung.com/cs/convert-rgb-to-grayscale
+ */
+export function greyscale(imageDataBlock){
+    let luminosity;
+    for(let i=0; i<imageDataBlock.data.length; i+=4){
+        luminosity = ~~(
+            (0.31 * imageDataBlock.data[i]) + 
+            (0.59 * imageDataBlock.data[i+1]) +
+            (0.11 * imageDataBlock.data[i+2]));
+        
+        imageDataBlock.data[i] = luminosity;
+        imageDataBlock.data[i+1] = luminosity;
+        imageDataBlock.data[i+2] = luminosity;
+    }
+
+    return imageDataBlock;
+}
+
