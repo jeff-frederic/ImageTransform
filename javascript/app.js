a212228edf46f55fc4bfc95765565ff478a4c4f3
jeff-PC <jefferyfrederic@gmail.com>
2023-04-29 01:22:12 -0700

    fixed uploading feature

--- javascript/app.js
+++ javascript/app.js
@@ -23,32 +23,72 @@ monalisaImage.src = 'assets/monalisa.jpg';
 let statueImage = new Image();
 statueImage.src = 'assets/statue.jpg';
 
+let input = document.getElementById('image_input');
+input.addEventListener('change', () => {
+    clearButtons();
+    let file = input.files[0];
+    let uploadedImage = new Image();
+    uploadedImage.src = URL.createObjectURL(file);
+    uploadedImage.onload = () => {
+        buildView(uploadedImage);
+    }
+});
+
 
 document.getElementById('portraitButton').addEventListener('click', () => {
+    clearButtons();
     portraitImage.onload = buildView(portraitImage);
+    document.getElementById('portraitButton').classList.add('active');
 });
 
 document.getElementById('flowerButton').addEventListener('click', () => {
+    clearButtons();
     flowerImage.onload = buildView(flowerImage);
+    document.getElementById('flowerButton').classList.add('active');
 });
 
 document.getElementById('buildingsButton').addEventListener('click', () => {
+    clearButtons();
     buildingsImage.onload = buildView(buildingsImage);
+    document.getElementById('buildingsButton').classList.add('active');
 });
 
 document.getElementById('twofaceButton').addEventListener('click', () => {
+    clearButtons();
     twofaceImage.onload = buildView(twofaceImage);
+    document.getElementById('twofaceButton').classList.add('active');
 });
 
 document.getElementById('sunsetButton').addEventListener('click', () => {
+    clearButtons();
     sunsetImage.onload = buildView(sunsetImage);
+    document.getElementById('sunsetButton').classList.add('active');
 });
 
 document.getElementById('monalisaButton').addEventListener('click', () => {
+    clearButtons();
     monalisaImage.onload = buildView(monalisaImage);
+    document.getElementById('monalisaButton').classList.add('active');
 });
 
 document.getElementById('statueButton').addEventListener('click', () => {
-    statueButton.onload = buildView(statueImage);
+    clearButtons();
+    statueImage.onload = buildView(statueImage);
+    document.getElementById('statueButton').classList.add('active');
 });
 
+
+window.onload = () => {
+    document.getElementById('portraitButton').click();
+}
+
+
+function clearButtons(){
+    document.getElementById('portraitButton').classList.remove('active');
+    document.getElementById('flowerButton').classList.remove('active');
+    document.getElementById('buildingsButton').classList.remove('active');
+    document.getElementById('twofaceButton').classList.remove('active');
+    document.getElementById('sunsetButton').classList.remove('active');
+    document.getElementById('monalisaButton').classList.remove('active');
+    document.getElementById('statueButton').classList.remove('active');
+}
